d7a48094c6709deed8b9d0ec928ca8d3b6876f42
SRAKIB17 <rakibulssc5@gmail.com>
2023-04-28 11:58:04 +0600

    'done'

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 //  For main div
-var PaginationMainDiv = document.createElement('div');
+const PaginationMainDiv = document.createElement('div');
 PaginationMainDiv.setAttribute('class', 'pagination-srakib')
 
 //2nd div
-var secondDiv = document.createElement('div');
+const secondDiv = document.createElement('div');
 secondDiv.setAttribute('class', 'button-group')
 
-var buttonGenerate = (className, style) => {
+let borderColor = 'red'
+let borderWidth = 5
+let buttonBgColor = 'blue'
+let buttonTextColor = 'white'
+let buttonSize = 32
+let font = 534;
+
+let buttonStyle = {
+    borderColor: borderColor,
+    borderWidth: borderWidth + 'px',
+    backgroundColor: buttonBgColor,
+    color: buttonTextColor,
+    height: buttonSize + 'px',
+    paddingLeft: buttonSize - 20 + 'px',
+    paddingRight: buttonSize - 20 + 'px',
+    fontSize: font + 'px'
+}
+
+const buttonGenerate = ({ className, style, html, mode }) => {
     let button = document.createElement('button')
     button.setAttribute('class', className)
+    button.style.borderColor = style.borderColor
+    button.style.borderWidth = style.borderWidth + 'px';
+    button.style.backgroundColor = style.buttonBgColor;
+    button.style.color = style.buttonTextColor;
+    button.style.height = style.buttonSize + 'px'
+    button.style.paddingLeft = style.buttonSize - 20 + 'px';
+    button.style.paddingRight = style.buttonSize - 20 + 'px';
+    button.style.fontSize = style.fontSize + 'px';
+    secondDiv.appendChild(button)
 }
 
 
@@ -16,6 +43,12 @@ var buttonGenerate = (className, style) => {
 
 
 //!  when current_page greater one (current_page > 1)
+
+buttonGenerate({
+    className: "button buttonPage",
+    style: buttonStyle,
+
+})
 
 //!  disable button current page 
 
